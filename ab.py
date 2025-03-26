2c1fa26d81981546aa95b7342ebb8b1bc6ac49e4
prity-pat <pritykumarijsr1235@gmail.com>
2025-03-26 12:31:42 +0530

    Update main.py

--- ab.py
+++ ab.py
@@ -1,44 +1,45 @@
 import dash
-from dash import html, dcc
-from dash.dependencies import Input,Output
-import plotly.express as px 
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
 import pandas as pd
 
 app = dash.Dash(__name__)
 
 data = {
-    'area':[100,200,300,400,500,600,700,800,900,950,1000],
-    'cost':[98,150,200,250,300,350,400,450,500,550,600]
+    'area' : [100, 200, 300, 400, 500],
+    'price' : [10, 20, 30, 40, 50]
 }
 
-df = pd.DataFrame(data)
+df= pd.DataFrame(data)
 
-app.layout = html.Div([html.H1('hello'),
-html.H2('area vs cost Graph'),
-dcc.Dropdown(
-    id = 'graph_type',
-    options = [
-        {'label': 'line plot', 'value' : 'line'},
-        {'label': 'Scatter Plot', 'value' : 'scatter'},
-        {'label': 'Bar plot', 'value': 'bar'}
-    ]
-),
-dcc.Graph(id = 'graph')], 
-style ={'textAlign': 'center', 'color' :'blue','background': 'lightblue'})
+
+app.layout = html.Div([html.H1('My Application'),
+   html.H2('Area vs Price Graph'),
+   dcc.Dropdown(
+       id = 'graph_type',
+         options = [
+              {'label': 'Scatter Plot', 'value': 'scatter'},
+              {'label': 'Line Plot', 'value': 'line'},
+                {'label': 'Bar Plot', 'value': 'bar'}
+         ],
+   ),
+   dcc.Graph(id= 'graph')],
+   style={'textAlign': 'center', 'color': 'red', 'backgroundColor': 'blue'})
 
 @app.callback(
-        Output('graph', 'figure'),
-        [Input('graph_type', ' value')]
-)
-
+    Output('graph', 'figure'), 
+    [Input('graph_type', 'value')])
 
 def update_graph(graph_type):
-    if graph_type == 'line':
-        fig = px.line(df, x='area', y = 'cost')
-    elif graph_type == 'scatter':
-        fig = px.scatter(df , x = 'area', y = 'cost')
+    if graph_type == 'scatter':
+        fig = px.scatter(df, x='area', y='price')
+    elif graph_type == 'line':
+        fig = px.line(df, x='area', y='price')
     else:
-        fig = px.bar(df, x = 'area' , y = 'cost')
-    return fig            
+        fig = px.bar(df, x='area', y='price')
 
-app.run(debug=True)
+    return fig
+
+app.run(debug= True)
